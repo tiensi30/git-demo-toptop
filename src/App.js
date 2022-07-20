d14dcd37e9f2660e0cbfafc6699a4a06f0ff07f8
tiensi30 <tienks789@gmail.com>
2022-07-20 20:17:01 +0700

    add

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Datvina />
+      <h1>d</h1>
     </div>
   );
 }
--- src/datvina.js
+++
@@ -1,5 +0,0 @@
-function Datvina(){
-   return <h1>Idol đạt vinaphone</h1>
-}
-
-export default Datvina
